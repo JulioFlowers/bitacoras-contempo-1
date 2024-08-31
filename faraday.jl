b8898a84642d9af7777a8b7271aea63b0bc9dfb3
Julio <julio_perez@ciencias.unam.mx>
2024-08-30 20:00:49 -0600

    Terminao

--- faraday.jl
+++ faraday.jl
@@ -6,6 +6,7 @@ using CSV # para cargar archivos csv
 using Plots # para gráficar
 using DataFrames # para convertir un CSV en DataFrame
 using LsqFit # Ajustes no lineales
+using Statistics #usar promedio
 using LaTeXStrings # Para escribir mates
 using LinearAlgebra # manipular matrices
 
@@ -46,6 +47,18 @@ PARTE EXPERIMENTAL
 BN = [data[1:11, "BN1"],
     data[1:11, "BN2"],
     data[1:11, "BN3"]]
+
+#error de B*l para SF-2
+errBIM1 = []
+
+for i in 1:3
+    er= []
+    for j in 2:11
+        dl = 0.0505*0.00005 + BN[i][j]*0.0001
+        push!(er, [dl, dl])
+    end
+    push!(errBIM1, er)
+end
 
 #theta registrada SF-2
 theta = [data[1:11, "°N1"] .* (pi / 180),
@@ -89,7 +102,7 @@ for i in 1:3
     img = plot(
         BN[i] .* 0.0505, theta[i],
         yerror=0.00872665,
-        xerror=0.00005*0.0505,
+        xerror=hcat(errBIM1[i]...),
         label="SF-02, l = 0.0505 m, medición: $(i)",
         seriestype=:scatter,
         ylabel=L"$\Delta \theta \ [rad]$",
@@ -118,6 +131,18 @@ end
 BNM2 = [data[1:11, "BN1M2"],
     data[1:11, "BN2M2"],
     data[1:11, "BN3M2"]]
+
+#error de B*l para SF-5
+errBIM2 = []
+
+for i in 1:3
+    er= []
+    for j in 2:11
+        dl = 0.0404*0.00005 + BNM2[i][j]*0.0001
+        push!(er, [dl, dl])
+    end
+    push!(errBIM2, er)
+end
 
 #theta registrada SF-5
 thetaM2 = [data[1:11, "°N1M2"] .* (pi / 180),
@@ -162,7 +187,7 @@ for i in 1:3
     img = plot(
         BNM2[i] .* 0.0404, thetaM2[i],
         yerror=0.00872665,
-        xerror=0.00005*0.0404,
+        xerror=hcat(errBIM2[i]...),
         label="SF-05, l = 0.0404 m, medición: $(i)",
         seriestype=:scatter,
         ylabel=L"$\Delta \theta \ [rad]$",
@@ -186,6 +211,15 @@ for i in 1:3
     # Exportar la gráfica
     savefig(img, "faradaymat2med$(i).png")
 end
+
+#= 
+------------------------------------------------
+Diferencia entre I_o con B=0, a la I mas cercana a Io 
+para cada cambio de B.
+------------------------------------------------
+=#
+
+#Para SF-2
 
 ITM1 = [data[1:11, "IN1"], data[1:11, "IN2"], data[1:11, "IN3"]]
 errITM1 = []
@@ -216,7 +250,8 @@ deltaI_plot = plot(
     dpi=620
 )
 
-Ber = [Ber[i]=[0.00005, 0.00005] for i in 1:10]
+Ber = [[0.00005, 0.00005] for i in 1:10]
+
 for i in 1:3
     # Gráfico con los errores
     plot!(
@@ -225,12 +260,66 @@ for i in 1:3
         deltaI[i],
         yerror = hcat(errITM1[i]...),
         xerror = hcat(Ber[i]...),
-        ylabel=L"$I_{0}-I_{approx} SF-02\ [Lux]$",
+        ylabel=L"$I_{0}-I_{approx}\ [Lux]$",
         xlabel=L"$ B \ [T] $",
-        title=L"$I_{0}-I_{approx}$, en función de $B$",
-        label=L"SF-2\ n: %$(i) \ ",
+        title=L"$I_{0}-I_{approx}$, en función de $B$, para SF-2",
+        label=L"n: %$(i),  $I_{o} =\ %$(IoM1[i]) \pm %$(round( (IoM1[i]*0.05)+2, digits = 0))$ Lux ",
         dpi=620
     )
 end
 
 display(deltaI_plot)
+savefig(deltaI_plot, "i_iomat1.png")
+
+
+#Para SF-2
+
+ITM2 = [data[1:11, "IN1M2"], data[1:11, "IN2M2"], data[1:11, "IN3M2"]]
+errITM2 = []
+
+for i in 1:3
+    er= []
+    for j in 2:11
+        dl = ((ITM2[i][j] * 0.05) + 2) * 2
+        push!(er, [dl, dl])
+    end
+    push!(errITM2, er)
+end
+
+IoM2 = [ITM2[i][1] for i in 1:3]
+IaM2 = [ITM2[i][2:11] for i in 1:3]
+
+# Calcular deltaI usando broadcasting para restar cada valor de IoM1 a su correspondiente en IaM1
+deltaI = [IaM2[i] .- IoM2[i] for i in 1:3]
+BNO = [BNM2[i][2:11] for i in 1:3]
+
+# Gráfico con los errores
+deltaI_plot = plot(
+    ylabel=L"$I_{0}-I_{approx}\ [Lux]$",
+    xlabel=L"$ B \ [T] $",
+    title=L"$I_{0}-I_{approx}$, en función de $B$",
+    legend=:outerbottom,
+     legendcolumns=3, 
+    dpi=620
+)
+
+Ber = [[0.00005, 0.00005] for i in 1:10]
+
+for i in 1:3
+    # Gráfico con los errores
+    plot!(
+        deltaI_plot,
+        BNO[i],
+        deltaI[i],
+        yerror = hcat(errITM2[i]...),
+        xerror = hcat(Ber[i]...),
+        ylabel=L"$I_{0}-I_{approx}\ [Lux]$",
+        xlabel=L"$ B \ [T] $",
+        title=L"$I_{0}-I_{approx}$, en función de $B$ para SF-5",
+        label=L"n: %$(i),  $I_{o} =\ %$(IoM2[i]) \pm %$(round( (IoM2[i]*0.05)+2, digits = 0))$ Lux ",
+        dpi=620
+    )
+end
+
+display(deltaI_plot)
+savefig(deltaI_plot, "i_iomat2.png")
